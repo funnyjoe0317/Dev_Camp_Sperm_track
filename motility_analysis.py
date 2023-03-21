f1f6c034b31f2d8acc3e28665bda8938cfd1dd52
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2023-03-21 17:49:01 +0900

    운동성 중복으로 값이 올라가는 문제해결 및 객체들의 움직임 비율로 결과값 출력 구현

--- motility_analysis.py
+++ motility_analysis.py
@@ -1,22 +1,15 @@
-import csv
 import numpy as np
 
 
-# ------------ min_track_length -> 프레임 길이에 맞게 설정 해줘야 함----------------
-def analyze_motility(tracks,  min_track_length=20):
+
+def analyze_motility(tracks, min_track_length=20):
     motility_results = {}
-    sperm_counts = {}
+    sperm_counts = {}  # 정자의 개수를 저장할 딕셔너리 추가
 
     video_name = "sample_video"
     video_tracks = tracks
 
-    motility_data = calculate_motility(video_tracks)
-    motility_results[video_name] = motility_data
-    
-    # # 고유한 트랙 ID 수를 세기
-    # sperm_count = len(set([track_id for track in video_tracks.values() for _, _, track_id in track]))
-    # sperm_counts[video_name] = sperm_count
-    # 고유한 트랙 ID 수를 세기 전에 트랙 길이 필터링
+    # 트랙 길이 필터링을 위한 코드 추가
     filtered_tracks = {}
     for frame, track_list in video_tracks.items():
         for track in track_list:
@@ -28,49 +21,53 @@ def analyze_motility(tracks,  min_track_length=20):
     # 최소 프레임 길이를 만족하는 트랙만 남기기
     long_tracks = {track_id: track for track_id, track in filtered_tracks.items() if len(track) >= min_track_length}
 
+
+    motility_data = calculate_motility(long_tracks)  # 수정된 long_tracks 전달
+    motility_results[video_name] = motility_data
+
+    # 정자의 개수를 저장
     sperm_counts[video_name] = len(long_tracks)
-  
-    return motility_results, sperm_counts
+    
+    return motility_results, sperm_counts  # 수정된 결과 반환
 
+def calculate_motility(long_tracks):
+    progressive_threshold = 30
+    non_progressive_threshold = 20
 
-def calculate_motility(video_tracks):
-    progressive_threshold = 10
-    non_progressive_threshold = 5
-
-    total_sperms = sum([len(track) for track in video_tracks.values()])
+    total_sperms = len(long_tracks)
     progressive_count = 0
     non_progressive_count = 0
     immotile_count = 0
 
-    distances = []  # 이동 거리를 저장할 리스트
-
-    for track in video_tracks.values():
-        for i in range(len(track) - 1):
-            current_position = np.array(track[i])
-            next_position = np.array(track[i + 1])
-            distance = np.linalg.norm(next_position - current_position)
-
-            distances.append(distance)  # 이동 거리를 리스트에 추가
-
-            if distance >= progressive_threshold:
-                progressive_count += 1
-                break
-            elif distance >= non_progressive_threshold:
-                non_progressive_count += 1
-                break
+    for track in long_tracks.values():
+        motility_type = calculate_total_distance(track, progressive_threshold, non_progressive_threshold)
+        if motility_type == 'progressive':
+            progressive_count += 1
+        elif motility_type == 'non_progressive':
+            non_progressive_count += 1
         else:
             immotile_count += 1
-
-    average_distance = np.mean(distances) if distances else 0
 
     motility_data = {
         "progressive": (progressive_count / total_sperms) * 100 if total_sperms > 0 else 0,
         "non_progressive": (non_progressive_count / total_sperms) * 100 if total_sperms > 0 else 0,
         "immotile": (immotile_count / total_sperms) * 100 if total_sperms > 0 else 0,
-        "average_distance": average_distance
     }
     return motility_data
-    
 
 
 
+def calculate_total_distance(track, progressive_threshold, non_progressive_threshold):
+    total_distance = 0
+    for i in range(len(track) - 1):
+        current_position = np.array(track[i])
+        next_position = np.array(track[i + 1])
+        distance = np.linalg.norm(next_position - current_position)
+        total_distance += distance
+
+    if total_distance >= progressive_threshold:
+        return 'progressive'
+    elif total_distance >= non_progressive_threshold:
+        return 'non_progressive'
+    else:
+        return 'immotile'
