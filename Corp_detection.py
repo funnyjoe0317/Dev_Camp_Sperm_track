27ea8f6a1b8d21993429ec0ac81e3ba0ab836573
Sungbum <funnyjoe0317@gmail.com>
2023-04-03 23:08:25 +0900

    배경제거 모델 사용 전

--- Corp_detection.py
+++ Corp_detection.py
@@ -1,4 +1,13 @@
 import cv2
+import numpy as np
+
+def remove_background(img):
+    fgmask = cv2.createBackgroundSubtractorMOG2().apply(img)
+    return fgmask
+
+def adjust_brightness(img, alpha=1.2, beta=0):
+    adjusted_img = cv2.addWeighted(img, alpha, np.zeros(img.shape, img.dtype), 0, beta)
+    return adjusted_img
 
 def preprocess_frame(frame, target_width, target_height):
     height, width = frame.shape[:2]
@@ -10,8 +19,11 @@ def preprocess_frame(frame, target_width, target_height):
     resized_gray = cv2.resize(center_gray, (target_width, target_height))
     
     equalized_gray = cv2.equalizeHist(resized_gray)
-
-    return equalized_gray
+    
+    adjusted_gray = adjust_brightness(equalized_gray, alpha=1.2, beta=0)
+    
+    # return equalized_gray
+    return adjusted_gray
 
 # def preprocess_frame(frame, target_width, target_height):
 #     height, width = frame.shape[:2]
@@ -45,7 +57,7 @@ def process_video(video_path, output_path):
     frame_skip = fps // 15
 
     target_width, target_height = 640, 480
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (target_width, target_height))
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (target_width, target_height), isColor=False)
 
     if not out.isOpened():
         print("Error opening the output video file")
@@ -55,7 +67,7 @@ def process_video(video_path, output_path):
         
     params = cv2.SimpleBlobDetector_Params()
     params.filterByArea = True
-    params.minArea = 10
+    params.minArea = 15
 
     detector = cv2.SimpleBlobDetector_create(params)
 
@@ -70,6 +82,7 @@ def process_video(video_path, output_path):
     prev_frame_gray = preprocess_frame(prev_frame_raw, target_width, target_height)
     prev_keypoints = detector.detect(prev_frame_gray)
     total_objects.extend(prev_keypoints)
+    out.write(prev_frame_gray)
     frame_count = 1
     while cap.isOpened():
         ret, frame = cap.read()
@@ -89,8 +102,9 @@ def process_video(video_path, output_path):
                 cv2.rectangle(now_frame_gray, (x - size, y - size), (x + size, y + size), (0, 255, 0), 2)
             prev_keypoints = now_keypoints
             out.write(now_frame_gray)
+            # 여기서 담기는 건데 왜 안담길까
             cv2.imshow('Processed Frame', cv2.cvtColor(now_frame_gray, cv2.COLOR_GRAY2BGR))
-            cv2.waitKey(0)
+            cv2.waitKey(1)
             
             print(f'Frame {frame_count} processed')
         frame_count += 1
@@ -98,7 +112,7 @@ def process_video(video_path, output_path):
     cap.release()
     out.release()
     
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
 
     num_moving_objects = len(moving_objects)
     num_total_objects = len(total_objects)
@@ -109,6 +123,8 @@ def process_video(video_path, output_path):
 
     return num_moving_objects, num_total_objects
 
+
+
 video_path = 'videos/7.mp4'
-output_path = 'videos/7_video_CORP.mp4'
+output_path = 'videos/7_video_CORP_brightness.mp4'
 moving_objects, total_objects = process_video(video_path, output_path)
