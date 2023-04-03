39fcb5fc17d9b54bf963a615bce7c40da2918177
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2023-04-03 17:47:05 +0900

    크롭과 전체 파일 완성전

--- Blobdetection_crop_WILL.py
+++ Blobdetection_crop_WILL.py
@@ -1,18 +1,35 @@
 import cv2
 
 def preprocess_frame(frame, target_width, target_height):
-    height, width, _ = frame.shape
-    roi_height, roi_width = height // 3, width // 3
+    height, width = frame.shape[:2]
 
-    center_roi = frame[roi_height:2*roi_height, roi_width:2*roi_width]
+    center_roi = frame[height//3:2*height//3, width//3:2*width//3]
 
     center_gray = cv2.cvtColor(center_roi, cv2.COLOR_BGR2GRAY)
     
-    equalized_gray = cv2.equalizeHist(center_gray)
+    resized_gray = cv2.resize(center_gray, (target_width, target_height))
+    
+    equalized_gray = cv2.equalizeHist(resized_gray)
 
-    resized_gray = cv2.resize(equalized_gray, (target_width, target_height))
+    return equalized_gray
 
-    return resized_gray, frame
+# def preprocess_frame(frame, target_width, target_height):
+#     height, width = frame.shape[:2]
+
+#     center_roi = frame[height//3:2*height//3, width//3:2*width//3]
+
+#     center_gray = cv2.cvtColor(center_roi, cv2.COLOR_BGR2GRAY)
+
+#     # Apply histogram equalization to increase contrast
+#     equalized_gray = cv2.equalizeHist(center_gray)
+
+#     # Apply binary thresholding to the image
+#     _, binary = cv2.threshold(equalized_gray, 127, 255, cv2.THRESH_BINARY)
+
+#     # Resize the image
+#     resized_binary = cv2.resize(binary, (target_width, target_height))
+
+#     return resized_binary
 
 def process_video(video_path, output_path):
     print("Opening video file...")
@@ -50,7 +67,7 @@ def process_video(video_path, output_path):
     if not ret:
         print("Error reading the first frame")
         return
-    prev_frame_gray, prev_frame = preprocess_frame(prev_frame_raw, target_width, target_height)
+    prev_frame_gray = preprocess_frame(prev_frame_raw, target_width, target_height)
     prev_keypoints = detector.detect(prev_frame_gray)
     total_objects.extend(prev_keypoints)
     frame_count = 1
@@ -60,7 +77,7 @@ def process_video(video_path, output_path):
             break
         if frame_count % frame_skip == 0:
             now_frame_raw = frame
-            now_frame_gray, now_frame  = preprocess_frame(now_frame_raw, target_width, target_height)
+            now_frame_gray = preprocess_frame(now_frame_raw, target_width, target_height)
 
             now_keypoints = detector.detect(now_frame_gray)
             moving_objects.extend([kp for kp in now_keypoints if not any([abs(prev_kp.pt[0] - kp.pt[0]) <= 5 and abs(prev_kp.pt[1] - kp.pt[1]) <= 5 for prev_kp in prev_keypoints])])
@@ -69,11 +86,11 @@ def process_video(video_path, output_path):
             for kp in now_keypoints:
                 x, y = int(kp.pt[0]), int(kp.pt[1])
                 size = int(kp.size)
-                cv2.rectangle(now_frame, (x - size, y - size), (x + size, y + size), (0, 255, 0), 2)
+                cv2.rectangle(now_frame_gray, (x - size, y - size), (x + size, y + size), (0, 255, 0), 2)
             prev_keypoints = now_keypoints
-            out.write(now_frame)
-            cv2.imshow('Processed Frame', now_frame)
-            cv2.waitKey(1)
+            out.write(now_frame_gray)
+            cv2.imshow('Processed Frame', cv2.cvtColor(now_frame_gray, cv2.COLOR_GRAY2BGR))
+            cv2.waitKey(0)
             
             print(f'Frame {frame_count} processed')
         frame_count += 1
@@ -92,7 +109,6 @@ def process_video(video_path, output_path):
 
     return num_moving_objects, num_total_objects
 
-video_path = 'videos/1.mp4'
-output_path = 'videos/1_video.mp4'
+video_path = 'videos/7.mp4'
+output_path = 'videos/7_video_CORP.mp4'
 moving_objects, total_objects = process_video(video_path, output_path)
-# 무엇이 문제인제 모르겠지만 현재 바운딩 박스가 좌측 상단에 나타나고 객체를 분할한 상태에서 객체 검출이 아닌 전체 화면이 나오면서 거기서 검출을 하는중이다.
